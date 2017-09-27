85b22e2c66279af436b7bb09b1e471d90283d0de
michal <gelhrid@gmail.com>
2017-09-27 17:58:34 +0200

    dodanie stronki nowej

--- server.js
+++ server.js
@@ -96,6 +96,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+    pageTitle: 'Projects Page!',
+    info: 'some info about projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'blad'
